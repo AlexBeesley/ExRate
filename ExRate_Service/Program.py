5b183a0a6f5d5789a3c26ab854ec5f994a0bfe9d
Alex <david.beesley1234@gmail.com>
2022-11-08 12:41:36 +0000

    added csv file with all available currency abvs for UI idiot proofing.

--- ExRate_Service/Program.py
+++ ExRate_Service/Program.py
@@ -1,8 +1,17 @@
+from pandas import *
 from DataVisualisation import GenerateGraphFromData
+
+
+data = read_csv("Assets/currency_codes.csv")
+abvs = data['AlphabeticCode'].tolist()
 
 print("Welcome to ExRate")
 base = input("""Please provide a base currency.
 >>> """)
 target = input("""Please provide a target currency.
 >>> """)
-GenerateGraphFromData.generateGraph(base, target)
+
+if target and base in abvs:
+    GenerateGraphFromData.generateGraph(base, target)
+else:
+    print("Please provide a correct Currency Abbreviation, e.g. EUR, GBP, USD etc.")
