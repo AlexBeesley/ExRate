40c9558ee9411673dcdc995e9c86cbbb8a255960
Alex <david.beesley1234@gmail.com>
2022-11-08 12:53:29 +0000

    program.py tweaks

--- ExRate_Service/Program.py
+++ ExRate_Service/Program.py
@@ -4,14 +4,20 @@ from DataVisualisation import GenerateGraphFromData
 
 data = read_csv("Assets/currency_codes.csv")
 abvs = data['AlphabeticCode'].tolist()
+running = True
+
+
+def GetUserInputs():
+    base = input("""Please provide a base currency: """)
+    target = input("""Please provide a target currency: """)
+    return base, target
+
 
 print("Welcome to ExRate")
-base = input("""Please provide a base currency.
->>> """)
-target = input("""Please provide a target currency.
->>> """)
-
-if target and base in abvs:
-    GenerateGraphFromData.generateGraph(base, target)
-else:
-    print("Please provide a correct Currency Abbreviation, e.g. EUR, GBP, USD etc.")
+while running:
+    base, target = GetUserInputs()
+    if target in abvs and base in abvs:
+        GenerateGraphFromData.generateGraph(base, target)
+    else:
+        print("Try again; provide a correct Currency Abbreviation, e.g. GBP, USD, EUR etc.")
+    print("\n")
