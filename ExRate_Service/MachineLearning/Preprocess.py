948184803f486802328dd338b6666d4934323d7c
Alex <david.beesley1234@gmail.com>
2022-11-08 16:36:35 +0000

    model progress

--- ExRate_Service/MachineLearning/Preprocess.py
+++ ExRate_Service/MachineLearning/Preprocess.py
@@ -10,72 +10,168 @@ from tensorflow.keras.preprocessing import timeseries_dataset_from_array
 from DataFetching import ProcessDataFromResponse
 
 
-rates, dates = ProcessDataFromResponse.processTimeSeries("GBP", "USD")
-rates = [float(x) for x in rates]
-data_array = np.array(rates)
+def plot_series(time, series, format="-", start=0, end=None):
+    """Helper function to plot our time series"""
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(False)
 
-split_fraction = 0.715
-train_split = int(split_fraction * int(data_array.shape[0]))
-step = 6
+def trend(time, slope=0):
+    """Define the trend through slope and time"""
+    return slope * time
 
-past = 365
-future = 14
-learning_rate = 0.001
-batch_size = 256
-epochs = 10
+def seasonal_pattern(season_time):
+    """Arbitrary definition of a seasonality pattern"""
+    return np.where(season_time < 0.1,
+                    np.cos(season_time * 6 * np.pi),
+                    2 / np.exp(9 * season_time))
 
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats a pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
 
-def normalize(data, train_split):
-    data_mean = data[:train_split].mean(axis=0)
-    data_std = data[:train_split].std(axis=0)
-    return (data - data_mean) / data_std
-
-
-features = normalize(data_array, train_split)
-features = pd.DataFrame(features)
-features.head()
-train_data = features.loc[0 : train_split - 1]
-val_data = features.loc[train_split:]
-
-start = past + future
-end = start + train_split
-
-x_train = train_data[[i for i in range(10)]].values
-y_train = features.iloc[10]
-
-print(x_train)
-print(y_train)
-quit()
-
-sequence_length = int(past / step)
-
-dataset_train = keras.preprocessing.timeseries_dataset_from_array(
-    x_train,
-    y_train,
-    sequence_length=sequence_length,
-    sampling_rate=step,
-    batch_size=batch_size,
-)
-
-x_end = len(val_data) - past - future
-
-label_start = train_split + past + future
-
-x_val = val_data.iloc[:x_end][[i for i in range(7)]].values
-y_val = features.iloc[label_start:][[1]]
-
-dataset_val = keras.preprocessing.timeseries_dataset_from_array(
-    x_val,
-    y_val,
-    sequence_length=sequence_length,
-    sampling_rate=step,
-    batch_size=batch_size,
-)
+def noise(time, noise_level=1, seed=None):
+    """Adds white noise to the series"""
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
 
 
-for batch in dataset_train.take(1):
-    inputs, targets = batch
+def generate_time_series():
+    # Temporal dimension: 4 years of data
+    time = np.arange(4 * 365 + 1, dtype="float32")
 
-print("Input shape:", inputs.numpy().shape)
-print("Target shape:", targets.numpy().shape)
-quit()
+    # The initial series is nothing more than a straight line which we will then modify with the other functions
+    y_intercept = 10
+    slope = 0.005
+    series = trend(time, slope) + y_intercept
+
+    # Add seasonality
+    amplitude = 50
+    series += seasonality(time, period=365, amplitude=amplitude)
+
+    # Add noise
+    noise_level = 3
+    series += noise(time, noise_level, seed=51)
+
+    return time, series
+
+class G:
+    TIME = np.arange(365, dtype="float32")
+    SERIES, time = ProcessDataFromResponse.processTimeSeries("GBP", "USD")
+    SPLIT_TIME = 1100
+    WINDOW_SIZE = 20
+    BATCH_SIZE = 32
+    SHUFFLE_BUFFER_SIZE = 1000
+
+def train_val_split(time, series, time_step=G.SPLIT_TIME):
+    time_train = time[:time_step]
+    series_train = series[:time_step]
+    time_valid = time[time_step:]
+    series_valid = series[time_step:]
+    return time_train, series_train, time_valid, series_valid
+
+
+def windowed_dataset(series, window_size=G.WINDOW_SIZE, batch_size=G.BATCH_SIZE, shuffle_buffer=G.SHUFFLE_BUFFER_SIZE):
+	dataset = tf.data.Dataset.from_tensor_slices(series)
+	dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+	dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+	dataset = dataset.shuffle(shuffle_buffer)
+	dataset = dataset.map(lambda window: (window[:-1], window[-1]))
+	dataset = dataset.batch(batch_size).prefetch(1)
+	return dataset
+
+
+time_train, series_train, time_valid, series_valid = train_val_split(G.TIME, G.SERIES)
+dataset = windowed_dataset(series_train)
+
+def create_uncompiled_model():
+    model = tf.keras.models.Sequential([
+      tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1),
+                    input_shape=[None]),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(1024, return_sequences=True)),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(512, return_sequences=True)),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(256, return_sequences=True)),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True)),
+      tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+      tf.keras.layers.Dense(1),
+  ])
+    return model
+
+
+class EarlyStopping(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if (logs.get('mae') < 0.03):
+            print("\nMAEthreshold reached. Training stopped.")
+            self.model.stop_training = True
+
+early_stopping = EarlyStopping()
+
+
+def create_model():
+    tf.random.set_seed(51)
+
+    model = create_uncompiled_model()
+
+    model.compile(loss=tf.keras.losses.Huber(),
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                  metrics=["mae"])
+    return model
+
+
+model = create_model()
+
+history = model.fit(dataset, epochs=20, callbacks=[early_stopping])
+
+plt.figure(figsize=(10, 6))
+plt.plot(history.history['mae'], label='mae')
+plt.plot(history.history['loss'], label='loss')
+plt.legend()
+#plt.show()
+
+def compute_metrics(true_series, forecast):
+    mse = tf.keras.metrics.mean_squared_error(true_series, forecast).numpy()
+    mae = tf.keras.metrics.mean_absolute_error(true_series, forecast).numpy()
+
+    return mse, mae
+
+def model_forecast(model, series, window_size):
+    ds = tf.data.Dataset.from_tensor_slices(series)
+    ds = ds.window(window_size, shift=1, drop_remainder=True)
+    ds = ds.flat_map(lambda w: w.batch(window_size))
+    ds = ds.batch(32).prefetch(1)
+    forecast = model.predict(ds)
+    return forecast
+
+
+# Prediction on the whole series
+all_forecast = model_forecast(model, G.SERIES, G.WINDOW_SIZE).squeeze()
+
+# Validation portion
+val_forecast = all_forecast[G.SPLIT_TIME - G.WINDOW_SIZE:-1]
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.plot(series_valid, label="validation set")
+plt.plot(val_forecast, label="predicted")
+plt.xlabel("Timestep")
+plt.ylabel("Value")
+plt.legend()
+plt.show()
+
+mse, mae = compute_metrics(series_valid, val_forecast)
+print(f"mse: {mse:.2f}, mae: {mae:.2f}")
+
+
+new_forecast = []
+
+new_forecast_series = G.SERIES[-G.WINDOW_SIZE:]
+
+pred = model.predict(new_forecast_series[np.newaxis])
+
+plt.figure(figsize=(15, 6))
+plt.plot(G.TIME[-100:], G.SERIES[-100:], label="last 100 points of time series")
+plt.scatter(max(G.TIME)+1, pred, color="red", marker="x", s=70, label="prediction")
+plt.legend()
+plt.show()
