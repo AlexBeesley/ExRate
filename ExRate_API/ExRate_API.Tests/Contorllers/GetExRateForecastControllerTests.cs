b8b4ba06eab8f99ba590cdd6fd426052bffc01f1
Alex <david.beesley1234@gmail.com>
2023-03-21 01:09:21 +0000

    test update for azure

--- ExRate_API/ExRate_API.Tests/Contorllers/GetExRateForecastControllerTests.cs
+++ ExRate_API/ExRate_API.Tests/Contorllers/GetExRateForecastControllerTests.cs
@@ -2,43 +2,38 @@ using ExRate_API.Controllers;
 using ExRate_API.DataFromService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
 using Moq;
 using System.Text.Json.Nodes;
 
 namespace ExRate_API.Tests.Controllers
 {
+    [TestFixture]
     public class GetExRateForecastControllerTests
     {
         private GetExRateForecastController _controller;
         private Mock<ILogger<GetExRateForecastController>> _loggerMock;
-        private Mock<IGetExRateForecast> _exRateForecastMock;
 
         [SetUp]
         public void Setup()
         {
+            // Create a mock logger object
             _loggerMock = new Mock<ILogger<GetExRateForecastController>>();
-            _exRateForecastMock = new Mock<IGetExRateForecast>();
 
+            // Create an instance of the controller with the mock logger
             _controller = new GetExRateForecastController(_loggerMock.Object);
         }
 
         [Test]
-        public void Get_ReturnsOkResult_WhenExRateForecastReturnsData()
+        public void Get_ReturnsJson()
         {
-            // Arrange
-            string baseCurrency = "USD";
-            string targetCurrency = "EUR";
-            string expectedOutput = "some data";
-
-            _exRateForecastMock.Setup(x => x.getOutput(baseCurrency, targetCurrency))
-                .Returns(expectedOutput);
-
             // Act
-            var result = _controller.Get(baseCurrency, targetCurrency);
+            var result = _controller.Get("USD", "EUR");
 
             // Assert
             Assert.IsInstanceOf<OkObjectResult>(result);
+            var okResult = (OkObjectResult)result;
+            Assert.IsInstanceOf<string>(okResult.Value);
         }
     }
 }
-
