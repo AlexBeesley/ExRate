a84620b8f350b2004c408946374996d83ac1d7b1
Alex <david.beesley1234@gmail.com>
2023-04-13 23:47:21 +0100

    using .env for key for reliablity.

--- ExRate_Service/DataPreprocessing/GetResponseFromAPI.py
+++ ExRate_Service/DataPreprocessing/GetResponseFromAPI.py
@@ -1,29 +1,15 @@
-import requests
 import os
-from azure.identity import DefaultAzureCredential, ClientSecretCredential
-from azure.keyvault.secrets import SecretClient
+import requests
+from dotenv import load_dotenv
 
 
 class GetResponseFromAPI:
     def __init__(self):
-        self.vault_url = "https://exchangerates-data.vault.azure.net/"
-        self.secret_name = "exchangerates-data"
-        self.credential = self.get_credential()
-        self.client = SecretClient(vault_url=self.vault_url, credential=self.credential)
+        load_dotenv()
         self.headers = {
-            "apikey": self.client.get_secret(self.secret_name).value
+            "apikey": os.getenv("API_KEY")
         }
         self.payload = {}
-
-    def get_credential(self):
-        if os.environ.get("DOTNET_RUNNING_IN_CONTAINER") == "true":
-            return ClientSecretCredential(
-                tenant_id="8f962d2b-fa24-43d2-be9c-887d97b9e926",
-                client_id="11378dec-77e9-4b44-aa09-8d747940b005",
-                client_secret="LNK8Q~v6llvpvZ1bkqn6JCeUYDjO21yhPiyq5bqf"
-            )
-        else:
-            return DefaultAzureCredential()
 
     def get_time_series(self, base, symbols, start_date, end_date):
         url = "https://api.apilayer.com/exchangerates_data/timeseries?base={}&symbols={}&start_date={}&end_date={}".format(
