5f229b29fb561957e0a400346e61c3b800941b54
Alex <david.beesley1234@gmail.com>
2023-04-16 15:53:16 +0100

    fix for azure.

--- ExRate_Service/DataPreprocessing/GetResponseFromAPI.py
+++ ExRate_Service/DataPreprocessing/GetResponseFromAPI.py
@@ -1,19 +1,16 @@
-import os
 import requests
-from dotenv import load_dotenv
 
 
 class GetResponseFromAPI:
     def __init__(self):
-        load_dotenv()
         self.headers = {
-            "apikey": os.getenv("API_KEY")
+            "apikey": "Sz8URYmcwtZwCogxzXPoFqXE0TCALKad"
         }
         self.payload = {}
 
     def get_time_series(self, base, symbols, start_date, end_date):
-        url = "https://api.apilayer.com/exchangerates_data/timeseries?base={}&symbols={}&start_date={}&end_date={}".format(
-            base, symbols, start_date, end_date)
+        url = "https://api.apilayer.com/exchangerates_data/timeseries?base={}&symbols={}&start_date={}&end_date={}"\
+            .format(base, symbols, start_date, end_date)
         response = requests.request("GET", url, headers=self.headers, data=self.payload)
         response.raise_for_status()
         return response.json()
