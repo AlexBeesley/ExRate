151f3c5dbeb4585f752dba48e006a93a938a38f5
Alex <david.beesley1234@gmail.com>
2023-04-16 15:42:21 +0100

    added hook

---
+++ ExRate_Frontend/src/Hooks/UseLoadingMessages.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export default function UseLoadingMessages(messages: string[], interval: number = 34285): string {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % messages.length);
+    }, interval);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [interval, messages.length]);
+
+  return messages[currentIndex];
+}
